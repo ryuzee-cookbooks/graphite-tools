a44d6822b7870383fcc8e1b367f1fad84f4eebd3
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-03-05 18:53:21 +0900

    add feature to change graphite password

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,7 @@
 default["graphite-tools"]["remove_before"] = 14 
+default["graphite-tools"]["graphite_bin"] = "/opt/graphite/bin"
 default["graphite-tools"]["whisper_path"] = "/opt/graphite/storage/whisper"
 default["graphite-tools"]["cron"]["hour"] = "0,6,12,18" 
 default["graphite-tools"]["cron"]["minute"] = "5" 
+default["graphite-tools"]["admin_user"] = "root"
+default["graphite-tools"]["admin_password"] = "password"
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,8 +15,12 @@ template "/usr/local/bin/#{filename}" do
   mode 0755
 end
 
-cron "remove_old_data" do
+cron "remove_unused_graphite_data" do
   hour "#{node['graphite-tools']['cron']['hour']}"
   minute "#{node['graphite-tools']['cron']['minute']}"
   command "/usr/local/bin/#{filename}"
+end
+
+execute "#{node["graphite-tools"]["graphite_bin"]}/set_admin_passwd.py #{node["graphite-tools"]["admin_user"]} #{node["graphite-tools"]["admin_password"]}" do
+  action :run
 end
